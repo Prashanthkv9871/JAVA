8a93bbeab5ce535333769084b4efb3c777e2b6f7
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-11-24 19:50:05 +0530

    java

---
+++ Access Modifier/Main.java
@@ -0,0 +1,10 @@
+class Test{
+    int a=200;
+}
+
+public class Main extends Test{
+    public static void main(String[] args){
+        Main t= new Main();
+        System.out.println(t.a);
+    }
+} 
